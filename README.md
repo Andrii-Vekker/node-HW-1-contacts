bd3901e08da62904cfb70d56c02b28d1ea39f3c0
Andrii-Vekker <vekker304@gmail.com>
2022-12-01 16:07:22 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-HW-1-contacts
+
+node index.js --action list - https://monosnap.com/file/WhG0szo1AlKbvk0R3kujkyuGTOpFHA
+
+node index.js --action get --id 5 -https://monosnap.com/file/nz9IynOrHjJPOGQCIkAeNCmtNjUAyT
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22 - https://monosnap.com/file/wx4D50VuGc2GJAteU1qGuZJn8932OB
+
+node index.js --action remove --id=3 - https://monosnap.com/file/brZk8YzZrO9vBeCr8X8NJvxUx7LiMm
--- contacts.js
+++ contacts.js
@@ -1,29 +1,58 @@
-const fs = require("fs").promisesж
+const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
+const { string } = require("yargs");
 
 
-const contactsPath = path.join(__dirname, "contacts.json")
-console.log(contactsPath)
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-// TODO: задокументировать каждую функцию
+const updateContacts = async(contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+
 const listContacts = async () => {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parce(data)
-}
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-function getContactById(contactId) {
-  // ...твой код
-}
+const getContactById = async (contactId) => {
+  // const id = String(contactId)  with yargs
+  const contacts = await listContacts();
+  // const result = contacts.find(it => it.id === id);  with yargs
+  const result = contacts.find(it => it.id === contactId);
+  return result || null;
+};
 
-function removeContact(contactId) {
-  // ...твой код
-}
+const removeCont = async (contactId) => {
+  // const id = String(contactId)  with yargs
+  const contacts = await listContacts();
+  // const index = contacts.findIndex(it => it.id === id);  with yargs
+  const index = contacts.findIndex(it => it.id === contactId);
+  if (index === -1) {
+    return null
+  };
+  const [result] = contacts.splice(index, 1);
+  //  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)); refactoring
+  await updateContacts(contacts)
+  return result
+};
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newBook = {
+    id: nanoid(),
+    name,
+    email,
+    phone
+  };
+  contacts.push(newBook);
+  await updateContacts(contacts)
+  return newBook;
+};
 
 
 module.exports = {
-    listContacts
-}
+  listContacts,
+  getContactById,
+  addContact,
+  removeCont
+};
